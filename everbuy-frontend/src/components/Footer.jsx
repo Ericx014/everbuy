258b0b5abf0ed966d273e6ec04dba61f81e90b2d
Eric Bong <ericbong6868@gmail.com>
2024-07-03 18:13:25 +0800

    Added footer

--- everbuy-frontend/src/components/Footer.jsx
+++ everbuy-frontend/src/components/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
 	return (
-		<footer className="static bottom-0 bg-white w-full shadow-lg h-[2rem] text-center">
-			Footer
-		</footer>
-	)
+    <footer className="static bottom-0 bg-white w-full shadow-lg h-[5.5rem] text-center flex flex-col justify-center">
+      <p>©Eric Bong 2024, All rights reserved.</p>
+    </footer>
+  );
 }
 
 export default Footer;
